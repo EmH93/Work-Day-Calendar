fd5d069084da63e8de3dacac39fe0a8204beee52
Emma Hayward <emmahayward93@gmail.com>
2023-01-19 17:12:15 +0000

    Added current day and full date to jumbotron header using jquery and moment.js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,8 @@
-// input current day into header
+// variables from moment.js - day, full date and current time.
 var currentDay = moment().format('dddd');
 var currentMonthDayYear = moment().format("MMMM Do YYYY"); 
 var currentTime = moment().format('h:mm:ss a');
 
-console.log(currentTime);
+//Adding current day and full date to jumbotron header
+var displayCurrentDayP = $('#currentDay');
+displayCurrentDayP.text(currentDay + ', ' + currentMonthDayYear);
