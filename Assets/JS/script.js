aec1a307158507f16d272f0b0f196a485c5f5bf0
Emma Hayward <emmahayward93@gmail.com>
2023-01-19 17:05:24 +0000

    added variable for current time with hour minute seconds to js file

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,6 @@
 // input current day into header
 var currentDay = moment().format('dddd');
 var currentMonthDayYear = moment().format("MMMM Do YYYY"); 
+var currentTime = moment().format('h:mm:ss a');
+
+console.log(currentTime);
