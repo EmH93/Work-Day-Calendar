ae4173829bee94ce9356a2f8f13745607b7ec837
Emma Hayward <emmahayward93@gmail.com>
2023-01-19 17:52:32 +0000

    replaced fakeTime with currentTime in if else statements - seems to work so far will have to look tomorrow to see it change as time changes.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,6 +3,8 @@ var currentDay = moment().format('dddd');
 var currentMonthDayYear = moment().format("MMMM Do YYYY"); 
 var currentTime = moment().format('HH:mm:ss');
 
+//TODO remove below once done: fake time variable so I can test the statements and log of current time
+var fakeTime = "08:23:33";
 console.log(currentTime);
 
 
@@ -22,69 +24,65 @@ var fivepmTextInput = $('#textEntry5pm');
     displayCurrentDayP.text(currentDay + ', ' + currentMonthDayYear);
 
 //if else statement that changes colour of input box - time dependant
+    //9am textinput colour
+        if(currentTime < "09:59:59" && currentTime > "08:59:59"){
+            nineamTextInput.attr("class", "form-control textarea present");
+        } else if(currentTime > "09:59:59") {
+            nineamTextInput.attr("class", "form-control textarea past" );
+        };
 
-//fake time variable so I can test the statements:
-var fakeTime = "08:23:33";
+    //10am textinput colour
+        if(currentTime < "10:59:59" && currentTime > "09:59:59"){
+        tenamTextInput.attr("class", "form-control textarea present");
+        } else if(currentTime > "10:59:59") {
+            tenamTextInput.attr("class", "form-control textarea past" );
+        };
 
-//9am textinput colour
-    if(fakeTime < "09:59:59" && fakeTime > "08:59:59"){
-        nineamTextInput.attr("class", "form-control textarea present");
-    } else if(fakeTime > "09:59:59") {
-        nineamTextInput.attr("class", "form-control textarea past" );
-    };
+    //11am textinput colour
+        if(currentTime < "11:59:59" && currentTime > "10:59:59"){
+            elevenamTextInput.attr("class", "form-control textarea present");
+        } else if(currentTime > "11:59:59") {
+            elevenamTextInput.attr("class", "form-control textarea past" );
+        };
 
-//10am textinput colour
-    if(fakeTime < "10:59:59" && fakeTime > "09:59:59"){
-       tenamTextInput.attr("class", "form-control textarea present");
-    } else if(fakeTime > "10:59:59") {
-        tenamTextInput.attr("class", "form-control textarea past" );
-    };
+    //12pm textinput colour
+        if(currentTime < "12:59:59" && currentTime > "11:59:59"){
+            twelvepmTextInput.attr("class", "form-control textarea present");
+        } else if(currentTime > "12:59:59") {
+            twelvepmTextInput.attr("class", "form-control textarea past" );
+        };
 
-//11am textinput colour
-    if(fakeTime < "11:59:59" && fakeTime > "10:59:59"){
-        elevenamTextInput.attr("class", "form-control textarea present");
-    } else if(fakeTime > "11:59:59") {
-        elevenamTextInput.attr("class", "form-control textarea past" );
-    };
+    //1pm textinput colour
+    if(currentTime < "13:59:59" && currentTime > "12:59:59"){
+            onepmTextInput.attr("class", "form-control textarea present");
+        } else if(currentTime > "13:59:59") {
+            onepmTextInput.attr("class", "form-control textarea past" );
+        };
 
-//12pm textinput colour
-    if(fakeTime < "12:59:59" && fakeTime > "11:59:59"){
-        twelvepmTextInput.attr("class", "form-control textarea present");
-    } else if(fakeTime > "12:59:59") {
-        twelvepmTextInput.attr("class", "form-control textarea past" );
-    };
+    //2pm textinput colour
+        if(currentTime < "14:59:59" && currentTime > "13:59:59"){
+            twopmTextInput.attr("class", "form-control textarea present");
+        } else if(currentTime > "14:59:59") {
+            twopmTextInput.attr("class", "form-control textarea past" );
+        };
 
-//1pm textinput colour
-if(fakeTime < "13:59:59" && fakeTime > "12:59:59"){
-        onepmTextInput.attr("class", "form-control textarea present");
-    } else if(fakeTime > "13:59:59") {
-        onepmTextInput.attr("class", "form-control textarea past" );
-    };
+    //3pm textinput colour
+        if(currentTime < "15:59:59" && currentTime > "14:59:59"){
+            threepmTextInput.attr("class", "form-control textarea present");
+        } else if(currentTime > "15:59:59") {
+            threepmTextInput.attr("class", "form-control textarea past" );
+        };
 
-//2pm textinput colour
-    if(fakeTime < "14:59:59" && fakeTime > "13:59:59"){
-        twopmTextInput.attr("class", "form-control textarea present");
-    } else if(fakeTime > "14:59:59") {
-        twopmTextInput.attr("class", "form-control textarea past" );
-    };
+    //4pm textinput colour
+        if(currentTime < "16:59:59" && currentTime > "15:59:59"){
+            fourpmTextInput.attr("class", "form-control textarea present");
+        } else if(currentTime > "16:59:59") {
+            fourpmTextInput.attr("class", "form-control textarea past" );
+        };
 
-//3pm textinput colour
-    if(fakeTime < "15:59:59" && fakeTime > "14:59:59"){
-        threepmTextInput.attr("class", "form-control textarea present");
-    } else if(fakeTime > "15:59:59") {
-        threepmTextInput.attr("class", "form-control textarea past" );
-    };
-
-//4pm textinput colour
-    if(fakeTime < "16:59:59" && fakeTime > "15:59:59"){
-        fourpmTextInput.attr("class", "form-control textarea present");
-    } else if(fakeTime > "16:59:59") {
-        fourpmTextInput.attr("class", "form-control textarea past" );
-    };
-
-//5pm textinput colour
-    if(fakeTime < "17:59:59" && fakeTime > "16:59:59"){
-        fivepmTextInput.attr("class", "form-control textarea present");
-    } else if(fakeTime > "17:59:59") {
-        fivepmTextInput.attr("class", "form-control textarea past" );
-    };
+    //5pm textinput colour
+        if(currentTime < "17:59:59" && currentTime > "16:59:59"){
+            fivepmTextInput.attr("class", "form-control textarea present");
+        } else if(currentTime > "17:59:59") {
+            fivepmTextInput.attr("class", "form-control textarea past" );
+        };
